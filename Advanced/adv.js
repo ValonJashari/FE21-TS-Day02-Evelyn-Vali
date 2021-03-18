f13a49bc196141919aaa76d47832ebd1b45f4de8
Valon Jashari <valonjashari18@gmail.com>
2021-03-18 13:19:26 +0100

    advance

---
+++ Advanced/adv.js
@@ -0,0 +1,66 @@
+/*Create an App that has to offer four different of types of vehicles and list the vehicle models based on the selected panel. Once the user clicks on some vehicle show its details and possibility to view the price
+
+Create a Parent Class called Vehicles
+Define properties and methods for the super Class
+Consider the inheritance concept
+Define 2 other Child classes and name them Motorbikes and Trucks
+Based on the personal vehicle performance model, kilometers left, number of seats, fuel type and year of production calculate the price once the user clicks on the button "show price" *use your dummy custom formula)
+
+*/
+var Vehicles = /** @class */ (function () {
+    function Vehicles(parameters) {
+    }
+    return Vehicles;
+}());
+/*
+var array: Array<{fName: string, lName: string, img: string, showMessage: Function}> = [];
+ class person1 {
+     fName:string;
+     lName:string;
+     img:string;
+     constructor(a:string,b,c){ //constructor a will not take the type of the fName from parameter of class person1
+         this.fName = a;
+         this.lName = b;
+         this.img = c;
+         console.log(this);
+         array.push(this);
+         console.table(array);
+     }
+
+     showMessage(){
+         return `   <p>${this.fName}</p>
+                    <p>${this.lName}</p>
+                    <img src="${this.img}">`
+     }
+ }
+ new person1("Vali", "Seri", "https://cdn.pixabay.com/photo/2015/09/09/16/05/forest-931706__340.jpg");
+new person1("The", "Vali", "https://cdn.pixabay.com/photo/2015/09/09/16/05/forest-931706__340.jpg");
+ new person1("Ai", "dddd","https://cdn.pixabay.com/photo/2015/09/09/16/05/forest-931706__340.jpg");
+
+ 
+class test extends person1{
+     salary: number;
+     constructor(a,b,c, num){
+         super(a,b,c);
+         this.salary = num;
+         //array.push(this);
+     }
+     showMessage(){
+         return `${super.showMessage()} <p>and the salary is ${this.salary}</p>`;
+         
+     }
+ }
+ new test("Vali", "test", "https://cdn.pixabay.com/photo/2015/09/09/16/05/forest-931706__340.jpg", 3000);
+ //var array = [person2, person3, person4];
+ console.table(array);
+
+ for (let val of array)
+ {
+    document.getElementById("demo").innerHTML += val.showMessage();
+ }
+
+
+ //document.getElementById("demo").innerHTML = person2.showMessage();
+ //document.getElementById("demo").innerHTML += person2.showMessage();
+ //document.getElementById("demo").innerHTML += person2.showMessage();
+ */ 
